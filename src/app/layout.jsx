1d37a7eeb4d43655419a3f73395c4ae15a04c093
SADMAN30102001SAKIB <evan30102001@gmail.com>
2024-10-14 01:50:15 +0600

    v1.8.3

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -104,10 +104,15 @@ export default function RootLayout({ children }) {
     const preventZoom = (e) => {
       e.preventDefault();
     };
+
     document.addEventListener("gesturestart", preventZoom);
+    document.addEventListener("gesturechange", preventZoom);
+    document.addEventListener("gestureend", preventZoom);
 
     return () => {
       document.removeEventListener("gesturestart", preventZoom);
+      document.removeEventListener("gesturechange", preventZoom);
+      document.removeEventListener("gestureend", preventZoom);
     };
   }, []);
 
