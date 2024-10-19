851488b9e3f860bdaddc2d435dc7570126f8aaf4
SADMAN30102001SAKIB <evan30102001@gmail.com>
2024-10-19 18:15:03 +0600

    v1.8.13

--- src/app/data/blogs.js
+++ src/app/data/blogs.js
@@ -4,7 +4,7 @@ const blogs = [
     description:
       "Learn how to turn $1K into $90M with compounding with 10% monthly growth.",
     date: "2024-10-15",
-    url: "https://nexusfuturefund2024.medium.com/turn-1k-into-90m-with-compounding-achieve-10-monthly-growth-98162605b511",
+    url: "https://nexusfuturefundofficial.medium.com/turn-1k-into-90m-with-compounding-achieve-10-monthly-growth-98162605b511",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*WMlEDVfDN1gmQwfYYHTZ7g.jpeg",
   },
   {
@@ -12,7 +12,7 @@ const blogs = [
     description:
       "An in-depth look at the essentials of cryptocurrency investments.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/understanding-cryptocurrency-investments-an-in-depth-look-at-the-essentials-8e5ce8f08dad",
+    url: "https://nexusfuturefundofficial.medium.com/understanding-cryptocurrency-investments-an-in-depth-look-at-the-essentials-8e5ce8f08dad",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*lkTIaENNCuR_f9vjAAGKfg.jpeg",
   },
   {
@@ -20,7 +20,7 @@ const blogs = [
     description:
       "Discover the top 5 strategies to maximize your crypto investments.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/top-5-crypto-investment-strategies-maximize-your-gains-8b01deca0cc0",
+    url: "https://nexusfuturefundofficial.medium.com/top-5-crypto-investment-strategies-maximize-your-gains-8b01deca0cc0",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*dGkuPKdxzZNSgf_PoK9PiA.jpeg",
   },
   {
@@ -28,7 +28,7 @@ const blogs = [
     description:
       "Learn how to manage and mitigate risks in cryptocurrency investments.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/managing-risk-in-crypto-investments-essential-strategies-to-protect-your-portfolio-c26e49dd2bc7",
+    url: "https://nexusfuturefundofficial.medium.com/managing-risk-in-crypto-investments-essential-strategies-to-protect-your-portfolio-c26e49dd2bc7",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*48c6NxTb1g86cZQCQEXFmQ.jpeg",
   },
   {
@@ -36,7 +36,7 @@ const blogs = [
     description:
       "Explore how blockchain technology is revolutionizing financial systems.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/the-future-of-blockchain-in-finance-revolutionizing-global-financial-systems-7bed0a59327b",
+    url: "https://nexusfuturefundofficial.medium.com/the-future-of-blockchain-in-finance-revolutionizing-global-financial-systems-7bed0a59327b",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Wn4Eg_gip1o2Uoko7FmxyA.jpeg",
   },
   {
@@ -44,7 +44,7 @@ const blogs = [
     description:
       "A comprehensive guide on cryptocurrency taxation and its impact on investors.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/crypto-taxation-what-investors-need-to-know-6078276656ff",
+    url: "https://nexusfuturefundofficial.medium.com/crypto-taxation-what-investors-need-to-know-6078276656ff",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*mCJkue-tRmrQIBoPqpnSEA.jpeg",
   },
   {
@@ -52,7 +52,7 @@ const blogs = [
     description:
       "Compare altcoins and Bitcoin to find out which is the better investment for your portfolio.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/altcoins-vs-bitcoin-whats-the-better-investment-fd6b9c84a4c5",
+    url: "https://nexusfuturefundofficial.medium.com/altcoins-vs-bitcoin-whats-the-better-investment-fd6b9c84a4c5",
     img: "https://miro.medium.com/v2/resize:fit:1400/format:webp/1*p2Moc0dJw_w7S-0pnnos5A.jpeg",
   },
   {
@@ -60,7 +60,7 @@ const blogs = [
     description:
       "Learn the warning signs of crypto scams and how to avoid them.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/how-to-spot-a-cryptocurrency-scam-learn-the-warning-signs-and-protect-your-investments-6361a5c0527e",
+    url: "https://nexusfuturefundofficial.medium.com/how-to-spot-a-cryptocurrency-scam-learn-the-warning-signs-and-protect-your-investments-6361a5c0527e",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*Can-yYRpe9Lw1BJYEoy8Og.jpeg",
   },
   {
@@ -68,7 +68,7 @@ const blogs = [
     description:
       "An introduction to decentralized finance (DeFi) and how it's reshaping the crypto landscape.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/defi-explained-a-beginners-guide-to-decentralized-finance-0af3d9480311",
+    url: "https://nexusfuturefundofficial.medium.com/defi-explained-a-beginners-guide-to-decentralized-finance-0af3d9480311",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*03OY7EeQxiMj4KuUMh-NKA.jpeg",
   },
   {
@@ -76,7 +76,7 @@ const blogs = [
     description:
       "A detailed comparison of staking and mining in cryptocurrency.",
     date: "2024-10-10",
-    url: "https://nexusfuturefund2024.medium.com/staking-vs-mining-which-is-more-profitable-55aa26da08b5",
+    url: "https://nexusfuturefundofficial.medium.com/staking-vs-mining-which-is-more-profitable-55aa26da08b5",
     img: "https://miro.medium.com/v2/resize:fit:1100/format:webp/1*WVFQbZVc34LXiWdmxpFreA.jpeg",
   },
 ];
