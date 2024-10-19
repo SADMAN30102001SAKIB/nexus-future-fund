879b2d2a8e20767295410e3f544a95feaae08fa9
SADMAN30102001SAKIB <evan30102001@gmail.com>
2024-10-20 05:37:12 +0600

    v2.0.7

--- src/app/wallet/login/page.jsx
+++ src/app/wallet/login/page.jsx
@@ -6,7 +6,7 @@ import { useState } from "react";
 const Login = () => {
   const [error, setError] = useState(null);
   let baseUrl = "https://nexusfuturefund.vercel.app";
-  if (typeof window !== "undefined") {
+  if (typeof window !== undefined) {
     baseUrl = "http://localhost:3000";
   }
 
