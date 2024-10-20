9d9f3b2dde40deb165aada065190d622eb659a4c
SADMAN30102001SAKIB <evan30102001@gmail.com>
2024-10-21 02:40:29 +0600

    v2.1.1

--- src/app/wallet/page.jsx
+++ src/app/wallet/page.jsx
@@ -67,10 +67,7 @@ export default function Wallet() {
         sessionUser = await account.get();
         setUser(sessionUser);
       } catch (error) {
-        console.log(
-          "User not authenticated or error fetching user data:",
-          error,
-        );
+        alert("User not authenticated or error fetching user data:", error);
         router.push("/wallet/login");
         return;
       }
