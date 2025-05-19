75f47b35320026121ebeccbf0ecc218ca318a3b3
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2025-05-19 06:19:15 +0600

    changed environment variable

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -2,7 +2,7 @@ import { Client, Databases, Account, Storage } from "appwrite";
 
 const client = new Client();
 
-client.setProject(process.env.PROJECT);
+client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
 
 const database = new Databases(client);
 const account = new Account(client);
