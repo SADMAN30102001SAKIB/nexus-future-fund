910f85c6fa5d8b06b9baa44d2006f018d28ab4c7
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2025-05-19 06:22:43 +0600

    modified environment variable setup

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -2,7 +2,13 @@ import { Client, Databases, Account, Storage } from "appwrite";
 
 const client = new Client();
 
-client.setProject(process.env.NEXT_PUBLIC_APPWRITE_PROJECT!);
+const projectId = process.env.NEXT_PUBLIC_APPWRITE_PROJECT;
+
+if (!projectId) {
+  throw new Error("Missing Appwrite project ID in environment variables.");
+}
+
+client.setProject(projectId);
 
 const database = new Databases(client);
 const account = new Account(client);
