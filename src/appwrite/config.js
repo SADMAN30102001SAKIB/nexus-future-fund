6873eed1c95bd114d6ebed6203d12fa7863e6b07
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2025-05-19 06:10:58 +0600

    added environment variable

--- src/appwrite/config.js
+++ src/appwrite/config.js
@@ -2,7 +2,7 @@ import { Client, Databases, Account, Storage } from "appwrite";
 
 const client = new Client();
 
-client.setProject("6714d4f1001449eb409f");
+client.setProject(process.env.PROJECT);
 
 const database = new Databases(client);
 const account = new Account(client);
