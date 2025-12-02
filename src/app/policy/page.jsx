b5efeccfe7c438d446cf0b239aea228445b5a09b
Sadman Sakib <90724482+SADMAN30102001SAKIB@users.noreply.github.com>
2025-12-02 16:03:08 +0600

    Update contact address in policy page

--- src/app/policy/page.jsx
+++ src/app/policy/page.jsx
@@ -252,7 +252,7 @@ export default function Policy() {
                 title: "Contact",
                 links: [
                   "nexusfuturefund@gmail.com",
-                  "Address: 4 Endsleigh Street, London, WC1H 0DS",
+                  "Address: Talaimari, Rajshahi, Bangladesh",
                 ],
               },
             ].map((column, index) => (
